aade2dbc78055f8021a73cd4c69c15a57621f465
Ronald Tse <ronald.tse@ribose.com>
2026-04-12 18:08:18 +0800

    feat: cross-reference resolution via xml_id_map lookup
    Add XrefResolver that builds an O(1) xml_id_map from parsed DocBook
    elements and resolves linkend references to target titles. Supports
    title, abbrev, and fallback text extraction for xref rendering.

---
+++ lib/docbook/xref_resolver.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+module Docbook
+  # Resolves xrefs in a DocBook document by building an O(1) xml:id lookup hash
+  # and resolving all xref/linkend references to their target titles.
+  class XrefResolver
+    def initialize(document)
+      @document = document
+      @xml_id_map = {}
+    end
+
+    # Build the xml:id -> element hash
+    def resolve!
+      @xml_id_map = build_xml_id_map(@document)
+      self
+    end
+
+    # Get the resolved title text for a linkend ID
+    def title_for(linkend)
+      target = @xml_id_map[linkend.to_s]
+      return nil unless target
+      best_title(target)
+    end
+
+    # Get element by xml:id
+    def [](xml_id)
+      @xml_id_map[xml_id.to_s]
+    end
+
+    private
+
+    def build_xml_id_map(el, map = {})
+      return map unless el.is_a?(Lutaml::Model::Serializable)
+
+      map[el.xml_id.to_s] = el if el.xml_id
+
+      # Walk child collections based on element type
+      case el
+      when Docbook::Elements::Book
+        el.part&.each { |p| build_xml_id_map(p, map) }
+        el.chapter&.each { |c| build_xml_id_map(c, map) }
+        el.appendix&.each { |a| build_xml_id_map(a, map) }
+        el.preface&.each { |p| build_xml_id_map(p, map) }
+        el.glossary&.each { |g| build_xml_id_map(g, map) }
+        el.bibliography&.each { |b| build_xml_id_map(b, map) }
+        el.index&.each { |i| build_xml_id_map(i, map) }
+      when Docbook::Elements::Article
+        el.section&.each { |s| build_xml_id_map(s, map) }
+      when Docbook::Elements::Part
+        el.chapter&.each { |c| build_xml_id_map(c, map) }
+        el.appendix&.each { |a| build_xml_id_map(a, map) }
+        el.reference&.each { |r| build_xml_id_map(r, map) }
+        el.glossary&.each { |g| build_xml_id_map(g, map) }
+        el.bibliography&.each { |b| build_xml_id_map(b, map) }
+        el.index&.each { |i| build_xml_id_map(i, map) }
+      when Docbook::Elements::Chapter, Docbook::Elements::Appendix,
+           Docbook::Elements::Section, Docbook::Elements::Preface
+        el.section&.each { |s| build_xml_id_map(s, map) }
+      when Docbook::Elements::Reference
+        el.refentry&.each { |r| build_xml_id_map(r, map) }
+      end
+
+      map
+    end
+
+    def best_title(el)
+      case el
+      when Docbook::Elements::Article, Docbook::Elements::Book
+        el.info&.title&.content
+      when Docbook::Elements::Section, Docbook::Elements::Chapter, Docbook::Elements::Appendix,
+           Docbook::Elements::Preface, Docbook::Elements::Part, Docbook::Elements::Reference
+        el.title&.content
+      else
+        el.title&.content rescue nil
+      end
+    end
+  end
+end
